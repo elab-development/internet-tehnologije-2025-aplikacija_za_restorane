1965daaa4f2a31c75045b04b308ca4fa7cf25f3b
anastasija585 <anastasic582@gmail.com>
2026-02-09 00:53:34 +0100

    Osnovna struktura aplikacije sa 3 stranice

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,7 +1,7 @@
 export default function ContactPage() {
   return (
     <main style={{ padding: 24 }}>
-      <h1>Kontakt:</h1>
+      <h1>Kontakt</h1>
 
       <p>
         Za pitanja u vezi rezervacija možete nas kontaktirati:
@@ -14,5 +14,6 @@ export default function ContactPage() {
     </main>
   );
 }
+
 
 
